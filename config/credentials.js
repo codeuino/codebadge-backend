021ce8543a6b71ae8dae3b57c7e23e5444ff5a65
Prabhani Nanayakkara <prabhaninanayakkara1907@gmail.com>
2019-05-20 03:41:40 +0530

    Migrate to Vue.js

--- config/credentials.js
+++
@@ -1,3 +0,0 @@
-module.exports={
-    database:"mongodb://localhost:27017"
-}
--- routes/index.js
+++
@@ -1,6 +0,0 @@
-const express=require('express')
-const router=express.Router()
-router.get('/test',(req,res)=>{
-    res.send('working sucessfully')
-})
-module.exports=router
