e4cc3777c563e825682e956a7b366b9a528c8acc
21tcoelho <58445187+21tcoelho@users.noreply.github.com>
2019-12-10 00:23:49 -0500

    Add files via upload

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import AuthView from './views/AuthView';
 import HomeView from './views/HomeView';
 import OrgView from './views/OrgView';
+import NewBadge from './views/NewBadge';
 import AuthService from './services/authService';
 
 Vue.use(Router);
@@ -29,12 +30,20 @@ export default new Router({
       }
     },
     {
+      path:'/newBadge',
+      name: 'newBadge',
+      component: NewBadge,
+      beforeEnter: (to, from, next) => {
+        next(authService.isLoggedIn());
+      }
+    },
+    {
       path: '/org/:name',
       name: 'orgView',
       component: OrgView,
       beforeEnter: (to, from, next) => {
         next(authService.isLoggedIn());
-      }
+      },
     }
   ]
 });
