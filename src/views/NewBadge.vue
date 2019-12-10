4894c7cd213003afcd542f8545b9136d6385a03b
21tcoelho <58445187+21tcoelho@users.noreply.github.com>
2019-12-10 00:22:57 -0500

    Add files via upload

---
+++ src/views/NewBadge.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class = "mt-5 orgs">
+  <h1><b>Create a New Badge!</b></h1>
+    <v-layout class="ma-5" justify-center>
+      <canvas width="900" height="600">
+      </canvas>
+    </v-layout>
+  </div>
+</template>
+
+<style>
+canvas {
+    background-color: white;
+    margin: auto;
+    display: block;
+    border: 3px solid gray;
+    border-radius: 2px;
+}
+</style>
