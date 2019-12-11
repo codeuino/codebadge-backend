6c23bb397fcbdf5e64422129931b7daf5fa80cbd
21tcoelho <58445187+21tcoelho@users.noreply.github.com>
2019-12-10 23:07:31 -0500

    Create BadgeButton.vue

---
+++ src/components/home/BadgeButton.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <v-btn class="btn-login" v-if="!isLoading" outline><a href = "#/newbadge">Create New Badge</a></v-btn>
+    <v-btn class="btn-login" v-if="isLoading" outline>
+    </v-btn>
+  </div>
+</template>
+
