4ed42beae2436721a74fc31c787c6d6889daa210
ayushnagar123 <an431999@gmail.com>
2019-12-14 02:55:57 +0530

    Flask api

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import AuthView from './views/AuthView';
 import HomeView from './views/HomeView';
 import OrgView from './views/OrgView';
+import Upload from "./views/Upload.vue";
 import AuthService from './services/authService';
 
 Vue.use(Router);
@@ -10,31 +11,35 @@ Vue.use(Router);
 const authService = new AuthService();
 
 export default new Router({
-  routes: [
-    {
-      path: '',
-      redirect: 'auth'
-    },
-    {
-      path: '/auth',
-      name: 'authView',
-      component: AuthView
-    },
-    {
-      path: '/home',
-      name: 'homeView',
-      component: HomeView,
-      beforeEnter: (to, from, next) => {
-        next(authService.isLoggedIn());
-      }
-    },
-    {
-      path: '/org/:name',
-      name: 'orgView',
-      component: OrgView,
-      beforeEnter: (to, from, next) => {
-        next(authService.isLoggedIn());
-      }
-    }
-  ]
+    routes: [{
+            path: '',
+            redirect: 'auth'
+        },
+        {
+            path: "/upload",
+            name: "Upload",
+            component: Upload
+        },
+        {
+            path: '/auth',
+            name: 'authView',
+            component: AuthView
+        },
+        {
+            path: '/home',
+            name: 'homeView',
+            component: HomeView,
+            beforeEnter: (to, from, next) => {
+                next(authService.isLoggedIn());
+            }
+        },
+        {
+            path: '/org/:name',
+            name: 'orgView',
+            component: OrgView,
+            beforeEnter: (to, from, next) => {
+                next(authService.isLoggedIn());
+            }
+        }
+    ]
 });
