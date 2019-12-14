e07a16a9a16b93085d0ff3463e52834def4fddaa
21tcoelho <58445187+21tcoelho@users.noreply.github.com>
2019-12-14 00:43:55 -0500

    Add files via upload

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import Router from 'vue-router';
 import AuthView from './views/AuthView';
 import HomeView from './views/HomeView';
 import OrgView from './views/OrgView';
-import Upload from "./views/Upload.vue";
+import NewBadge from './views/NewBadge';
 import AuthService from './services/authService';
 
 Vue.use(Router);
@@ -11,35 +11,39 @@ Vue.use(Router);
 const authService = new AuthService();
 
 export default new Router({
-    routes: [{
-            path: '',
-            redirect: 'auth'
-        },
-        {
-            path: "/upload",
-            name: "Upload",
-            component: Upload
-        },
-        {
-            path: '/auth',
-            name: 'authView',
-            component: AuthView
-        },
-        {
-            path: '/home',
-            name: 'homeView',
-            component: HomeView,
-            beforeEnter: (to, from, next) => {
-                next(authService.isLoggedIn());
-            }
-        },
-        {
-            path: '/org/:name',
-            name: 'orgView',
-            component: OrgView,
-            beforeEnter: (to, from, next) => {
-                next(authService.isLoggedIn());
-            }
-        }
-    ]
+  routes: [
+    {
+      path: '',
+      redirect: 'auth'
+    },
+    {
+      path: '/auth',
+      name: 'authView',
+      component: AuthView
+    },
+    {
+      path: '/home',
+      name: 'homeView',
+      component: HomeView,
+      beforeEnter: (to, from, next) => {
+        next(authService.isLoggedIn());
+      }
+    },
+    {
+      path:'/newBadge',
+      name: 'newBadge',
+      component: NewBadge,
+      beforeEnter: (to, from, next) => {
+        next(authService.isLoggedIn());
+      }
+    },
+    {
+      path: '/org/:name',
+      name: 'orgView',
+      component: OrgView,
+      beforeEnter: (to, from, next) => {
+        next(authService.isLoggedIn());
+      },
+    }
+  ]
 });
