0423525024db4c10a1aed2991dec214e5b803054
21tcoelho <21tcoelho@mcstudent.org>
2019-12-22 23:14:31 -0500

    Pencil Tool

---
+++ src/components/newBadge/ToolButtons.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+  <v-layout justify-center row>
+		<v-btn id="selector" color="black" dark>
+			<i class="fas fa-mouse-pointer"></i>
+		</v-btn>
+    <v-btn id="pencil" color="black" dark>
+			<i class="fas fa-pen"></i>
+		</v-btn>
+    <v-btn id="eraser" color="black" dark>
+			<i class="fas fa-eraser"></i>
+		</v-btn>
+    <v-btn id="square" color="black" dark>
+			<i class="fas fa-square"></i>
+		</v-btn>
+    <v-btn id="circle" color="black" dark>
+			<i class="fas fa-circle"></i>
+		</v-btn>
+    <v-btn id="magic" color="black" dark>
+			<i class="fas fa-magic"></i>
+		</v-btn>
+		<v-btn id="text" color="black" dark>T</v-btn>
+</v-layout>
+</div>
+</template>
+
+<script>
+
+  var selector = document.getElementById("selector");
+  var pencil = document.getElementById("pencil");
+  var eraser = document.getElementById("eraser");
+  var square = document.getElementById("square");
+  var circle = document.getElementById("circle");
+  var magic = document.getElementById("magic");
+  var text = document.getElementById("text");
+</script>
+
--- src/views/NewBadge.vue
+++ src/views/NewBadge.vue
@@ -1,54 +1,127 @@
 <template>
-  <body style ="background-image: url('https://png.pngtree.com/thumb_back/fh260/background/20190331/pngtree-vector-white-abstract-background-design-templates-collection-wit-image_94438.jpg'); background-repeat: repeat;">
-    <div class = "mt-5 orgs">
-      <h1 style ="font-family: georgia; font-size: 45px;">
-        <center>
-          <b>Create a New Badge!</b>
-        </center>
-      </h1>
-      <h2 style ="margin-bottom: 30px; font-family: georgia; font-size: 25px;">
-        <center>
+    <body style ="background-image: url('https://png.pngtree.com/thumb_back/fh260/background/20190331/pngtree-vector-white-abstract-background-design-templates-collection-wit-image_94438.jpg'); background-repeat: repeat; padding-top: 48px; padding-bottom: 25px;">
+      <div>
+        <h1 style ="font-family: georgia; font-size: 45px; text-align: center;">
+            <b>Create a New Badge!</b>
+        </h1>
+        <h2 style ="margin-bottom: 30px; font-family: georgia; font-size: 25px; text-align: center;">
           <b>Design your own badge for your organization.</b>
-        </center>
-      </h2>
-      <Buttons/>
-      <v-layout style="margin: 10px;" justify-center="">
-        <canvas id = "canvas" width="900" height="600">
-        </canvas>
-      </v-layout>
-    </div>
-  </body>
+        </h2>
+          <div>
+            <DrawButtons/>
+              <canvas id = "canvas" width="900" height="600">
+              </canvas>
+          </div>
+        </div>
+        <ToolButtons/>
+      </div>
+    </body>
 </template>
 
+<style>
+
+  canvas {
+  background-color: white;
+  border: 2px solid gray;
+  border-radius: 2px;
+  margin-bottom: 10px;
+  }
+
+
+</style>
+
 <script>
-  import Buttons from '../components/newBadge/Buttons';
+  import ToolButtons from '../components/newBadge/ToolButtons';
+  import DrawButtons from '../components/newBadge/DrawButtons';
 
   export default {
   name: 'NewBadge',
   components: {
-  Buttons
+  DrawButtons,
+  ToolButtons,
   }
   }
-  
-  function draw() {
-  var canvas = document.getElementById('canvas');
-  if (canvas.getContext) {
-  var ctx = newCanvas.getContext('2d');
+
+
+
+  window.addEventListener("load", () => {
+  const canvas = document.getElementById("canvas");
+
+  const ctx = canvas.getContext("2d");
+
+  function getMousePos(canvas, event) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+  x: (event.clientX - rect.left) / (rect.right - rect.left) * canvas.width,
+  y: (event.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
+  };
+  }
+
+  ctx.strokeStyle = "#000000";
+  ctx.lineWidth = 10;
+  ctx.lineCap = "round";
+
+  let shouldPaint = false;
+
+  function startDrawing() {
+  shouldPaint = true;
+  var mousePos = getMousePos(canvas, event);
+  ctx.moveTo(mousePos.x, mousePos.y);
+  ctx.beginPath()
+  }
+
+  function continueDrawing(event) {
+  if (shouldPaint) {
+  var mousePos = getMousePos(canvas, event);
+  ctx.lineTo(mousePos.x, mousePos.y)
+  ctx.stroke();
   }
   }
+
+  function endDrawing() {
+  shouldPaint = false;
+  }
+
+  //Allows user to draw on canvas
+  drawing();
+
+  function drawing(event) {
+  canvas.addEventListener("mousedown", startDrawing);
+  document.addEventListener("mouseup", endDrawing);
+  canvas.addEventListener("mousemove", continueDrawing);
+  }
+
+  // Changing colors
+  document.querySelectorAll(".color").forEach(link => {
+  link.addEventListener("click", function(event) {
+  ctx.strokeStyle = this.style.backgroundColor;
+  })
+  })
+
+  //Changing brush size
+  document.querySelectorAll(".size").forEach(link => {
+  link.addEventListener("click", function(event) {
+  ctx.lineWidth = this.id;
+  })
+  })
+
+
+
+  //Activates and deactivates buttons
+  function removeDraw(event) {
+  canvas.removeEventListener("mousedown", startDrawing);
+  console.log("working");
+  }
+
+  selector.addEventListener("click", removeDraw);
+  pencil.addEventListener("click", drawing);
+  eraser.addEventListener("click", removeDraw);
+  square.addEventListener("click", removeDraw);
+  circle.addEventListener("click", removeDraw);
+  magic.addEventListener("click", removeDraw);
+  text.addEventListener("click", removeDraw);
+
+
+  })
+
 </script>
-
-<style>
-  canvas {
-  background-color: white;
-  margin: auto;
-  display: block;
-  border: 2px solid gray;
-  border-radius: 2px;
-  }
-
-  .title {
-  padding: 20px;
-  font-size: 80px;
-  }
-</style>
