c1ee307880bcc50f227bd8d85fbf4fb62f30c75d
ayushnagar123 <an431999@gmail.com>
2020-06-18 16:30:40 +0530

    old code depriciated added new backend code with express setup

---
+++ config/config-sample.js
@@ -0,0 +1,11 @@
+module.exports={
+    github:{
+        clientId:"<github oauth app client id>",
+        clientSecret:"<github oauth app client secret>"
+    },
+    gmail:{
+        clientId:"<google oauth app client id>",
+        clientSecret:"<google oauth app client secret>"
+    }
+}
+
--- frontend/src/config/AxiosHelper.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  baseUrl: 'https://api.github.com',
-  authUrl: 'https://github.com/login/oauth/authorize',
-  gatekeeperUrl: 'https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/access_token',
-  homeUrl: 'http://localhost:8080/#/home',
-  homeAuthUrl: 'http://localhost:8080/#/auth'
-};
