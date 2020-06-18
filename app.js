f6b1b79a1454111edbf18cb4fa5429d52eaf9300
ayushnagar123 <an431999@gmail.com>
2020-06-18 23:10:35 +0530

    user profile fetch and organization fetch using github api

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var orgsRouter = require('./routes/orgs/');
 var usersRouter = require('./routes/users/');
@@ -14,6 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/users/githubAPI.js
+++ routes/users/githubAPI.js
@@ -6,6 +6,134 @@ const {github} = require('./../../config/config')
 var clientId=github.clientId
 var clientSecret=github.clientSecret
 
+router.get('/getUserProfile',(req,res)=>{
+    username = req.query.username
+    axios
+        .get(`https://api.github.com/users/${username}`)
+        .then(resp => {
+          res.send(resp.data)
+        })
+        .catch(err=>res.json({"error":err}))
+  })
+  
+  router.get('/getUserOrgs',(req,res)=>{
+      oauth_token=req.query.token
+      username=req.query.username
+      githubLogin=req.query.githubLogin
+      console.log(githubLogin)
+      console.log("username",req.query.username)
+      if(githubLogin=="false"){
+        console.log("! githubLogin is ",githubLogin)
+        axios
+        .get(`https://api.github.com/users/${username}/orgs`)
+        .then(resp => {
+          res.send(resp.data)
+        })
+        .catch(err=>res.json({"error":err}))
+      }
+      else{
+        console.log("githubLogin",githubLogin)
+        let config = {
+          headers: {
+            "Authorization":`token ${oauth_token}`,
+          }
+        }
+        console.log(config)
+        axios
+        .get(`https://api.github.com/user/orgs`,config=config)
+        .then(resp => {
+          console.log(resp.data.length)
+          return resp.data
+        })
+        .then(orgs=>{
+          var list_orgs=orgs;
+          let config = {
+            headers: {
+              "Authorization":`token ${oauth_token}`
+            }
+          }
+          axios
+          .get(`https://api.github.com/users/${username}/orgs`,config=config)
+          .then(resp1 => {
+            console.log(resp1.data.length)
+            var flag,elem;
+            resp1.data.forEach(element => {
+              flag=0;
+              elem=element
+              list_orgs.forEach(element1=>{
+                if(element1.login==element.login){
+                  flag=1;
+                }
+              })
+              if(flag==0){
+                list_orgs.push(elem);
+              }
+            });
+            res.send(list_orgs)
+          })
+        })
+        .catch(err=>{console.log(err);res.json({"error":err})})
+      }
+  })
+  
+  router.get('/getUserRepos',(req,res)=>{
+      oauth_token = req.query.token
+      let config = {
+        headers: {
+          "Authorization":`token ${oauth_token}`,
+        }
+      }
+      console.log(config)
+      username = req.query.username
+      if(username!==undefined){
+        axios
+        .get(`https://api.github.com/users/${username}/repos`,config=config)
+        .then(resp => {
+          res.send(resp.data)
+        })
+        .catch(err=>res.json({"error":err}))
+      }
+      else{
+        axios
+        .get(`https://api.github.com/user/repos`,config=config)
+        .then(resp => {
+          res.send(resp.data)
+        })
+        .catch(err=>res.json({"error":err}))
+      }
+  })
+  
+  router.get('/getUserData',(req,res)=>{
+      var oauth_token=req.query.token;
+      var username=req.query.username;
+      var githubLogin = req.query.githubLogin;
+      if(githubLogin=="true"){
+        console.log({"Authorization": `token ${oauth_token}`})
+        let config = {
+          headers: {
+            "Authorization":`token ${oauth_token}`
+          }
+        }
+        axios
+          .get(`https://api.github.com/user`,config=config)
+          .then(resp => {
+            res.cookie('username',resp.data["login"],{expires: new Date(Date.now() +  + 315360000000)}) //saves username for upcoming queries of logedin user
+            res.json(resp.data)
+          })
+          .catch(err=>res.sendStatus(400).json({"error":err}))
+      }
+      else{
+        console.log("not github login")
+        axios
+        .get(`https://api.github.com/users/${username}`)
+        .then(resp => {
+          res.cookie('username',resp.data["login"],{expires: new Date(Date.now() +  + 315360000000)}) //saves username for upcoming queries of logedin user
+          res.json(resp.data)
+        })
+        .catch(err=>res.json({"error":err}))
+      }
+    })
+
 router.get('/auth',(req,res)=>{
     const body = {
       client_id: clientId,
@@ -57,7 +185,6 @@ router.get('/auth',(req,res)=>{
             }
           })
           .catch(err=>res.sendStatus(400).json({"error":err}))
-        
       }).
       catch(err => {
         res.status(500).json({"error":err});
